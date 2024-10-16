f7c67f645536011ed20a4fbde6a2e3f38d776b80
Fly596 <69361885+Fly596@users.noreply.github.com>
2024-10-16 11:22:32 +0300

    feat: Add Medication screen UI and data structure
    This commit implements the UI for the Add Medication screen and introduces corresponding data structures:
    
    - Adds UI elements for inputting medication details such as name, type, strength, and notes to `MedicationsScreen`.
    - Creates a temporary data class `TEMP_Medication` to hold medication information for now.
    - Introduces a `dateTime` field in `TEMP_Medication` to store the date and time of medication entry.
    - Adds a button to fetch medication data and update the display.
    - Updates the `Medication` data class to reflect the new fields.
    - Implements a simple `CardComponent` to display medication information in a list.

--- medicationstracker/src/main/java/com/galeria/medicationstracker/data/DBCollections.kt
+++ medicationstracker/src/main/java/com/galeria/medicationstracker/data/DBCollections.kt
@@ -1,66 +1,73 @@
 package com.galeria.medicationstracker.data
 
-import android.os.Parcel
-import android.os.Parcelable
+import android.icu.text.SimpleDateFormat
 import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
-
+import java.util.Date
+import java.util.Locale
 
 // Contains data from firestore db.
 data class Medication(
-  val uid: String,
-  val name: String,
-  val type: String,
-  val form: MedicationForm = MedicationForm.TABLET,
-  val strength: Float = 0.0f,
-  val unit: MedicationUnit = MedicationUnit.MG,
-  val startDate: LocalDate = LocalDate.now(),
-  val endDate: LocalDate? = null,
-  val frequency: Frequency =  Frequency.AtRegularIntervals(0), // TODO: enum?
-  val intakeTime: LocalTime = LocalTime.now(),
-  val notes: String = "",
+    val uid: String,
+    val name: String,
+    val type: String,
+    val form: MedicationForm = MedicationForm.TABLET,
+    val strength: Float = 0.0f,
+    val unit: MedicationUnit = MedicationUnit.MG,
+    val startDate: LocalDate = LocalDate.now(),
+    val endDate: LocalDate? = null,
+    val frequency: Frequency = Frequency.AtRegularIntervals(0), // TODO: enum?
+    val intakeTime: LocalTime = LocalTime.now(),
+    val notes: String = "",
 )
 
 sealed class Frequency {
   data class AtRegularIntervals(val interval: Int) : Frequency()
+
   data class OnSpecificDaysOfTheWeek(val days: List<DayOfWeek>) : Frequency()
 }
 
-
 enum class MedicationForm {
-  TABLET, CAPSULE, LIQUID, INJECTION,
+  TABLET,
+  CAPSULE,
+  LIQUID,
+  INJECTION,
 }
 
 enum class MedicationUnit {
-  MG, ML, G,
+  MG,
+  ML,
+  G,
 }
 
+val sdf = SimpleDateFormat("MMMM dd, yyyy", Locale.US)
 
 data class TEMP_Medication(
-  val uid: String = "",
-  val name: String = "",
-  val type: String = "",
+    val uid: String = "",
+    val name: String = "",
+    val type: String = "",
+    val dateTime: String? = sdf.format(Date())
 )
 
 /*TODO: Symptoms, mood..*/
 data class MoodLog(
-  val date: LocalDateTime = LocalDateTime.now()
-  // TODO
+    val date: LocalDateTime = LocalDateTime.now()
+    // TODO
 )
 
 data class Users(
-  val name: String = "",
-  val email: String = "",
-  val pfp: String = "" /* url to pic*/,
-  val born: String = "" /*yyyy-MM-dd*/,
-  val notificationPreferences: NotificationPreferences = NotificationPreferences()
+    val name: String = "",
+    val email: String = "",
+    val pfp: String = "" /* url to pic*/,
+    val born: String = "" /*yyyy-MM-dd*/,
+    val notificationPreferences: NotificationPreferences = NotificationPreferences()
 )
 
 data class NotificationPreferences(
-  val sound: Boolean = true,
-  val vibration: Boolean = true,
+    val sound: Boolean = true,
+    val vibration: Boolean = true,
 )
 
 /* data class UserMedication(
@@ -71,4 +78,3 @@ data class NotificationPreferences(
   val frequency: String = "",
   val time: LocalTime = LocalTime.now(),
 ) */
-
--- medicationstracker/src/main/java/com/galeria/medicationstracker/ui/screens/medications/MedicationsScreen.kt
+++ medicationstracker/src/main/java/com/galeria/medicationstracker/ui/screens/medications/MedicationsScreen.kt
@@ -14,10 +14,16 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DisplayMode
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -26,6 +32,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.galeria.medicationstracker.R
@@ -38,8 +45,8 @@ import kotlinx.coroutines.runBlocking
 
 @Composable
 fun MedicationsScreen(
-  modifier: Modifier = Modifier,
-  viewModel: MedicationsViewModel = viewModel(),
+    modifier: Modifier = Modifier,
+    viewModel: MedicationsViewModel = viewModel(),
 ) {
   val uid = viewModel.userId
 
@@ -48,136 +55,147 @@ fun MedicationsScreen(
 
   Column {
     Text(
-      stringResource(R.string.meds_screen_title),
-      style = MaterialTheme.typography.headlineMedium,
+        stringResource(R.string.meds_screen_title),
+        style = MaterialTheme.typography.headlineMedium,
     )
 
     Spacer(modifier = Modifier.padding(8.dp))
 
     MyTextField(
-      value = viewModel.uiState.name,
-      onValueChange = { viewModel.updateMedName(it) },
-      label = "Medication name",
-      modifier = Modifier
-        .fillMaxWidth(),
+        value = viewModel.uiState.name,
+        onValueChange = { viewModel.updateMedName(it) },
+        label = "Medication name",
+        modifier = Modifier.fillMaxWidth(),
     )
     MyTextField(
-      value = viewModel.uiState.type,
-      onValueChange = { viewModel.updateMedType(it) },
-      label = "Medication type",
-      modifier = Modifier
-        .fillMaxWidth(),
+        value = viewModel.uiState.type,
+        onValueChange = { viewModel.updateMedType(it) },
+        label = "Medication type",
+        modifier = Modifier.fillMaxWidth(),
     )
     MyTextField(
-      value = viewModel.uiState.strength.toString(),
-      onValueChange = { viewModel.updateMedStrength(it.toFloat()) },
-      label = "Strength",
-      modifier = Modifier
-        .fillMaxWidth(),
+        value = viewModel.uiState.strength.toString(),
+        onValueChange = { viewModel.updateMedStrength(it.toFloat()) },
+        label = "Strength",
+        modifier = Modifier.fillMaxWidth(),
     )
     MyTextField(
-      value = viewModel.uiState.notes,
-      onValueChange = { viewModel.updateMedNotes(it) },
-      label = "Notes",
-      modifier = Modifier
-        .fillMaxWidth(),
+        value = viewModel.uiState.notes,
+        onValueChange = { viewModel.updateMedNotes(it) },
+        label = "Notes",
+        modifier = Modifier.fillMaxWidth(),
     )
 
     val context = LocalContext.current
 
     HIGButton(
-      text = "Submit",
-      onClick = {
-        val newMed = TEMP_Medication(
-          uid = uid,
-          name = viewModel.uiState.name,
-          type = viewModel.uiState.type,
-        )
+        text = "Submit",
+        onClick = {
+          val newMed =
+              TEMP_Medication(
+                  uid = uid,
+                  name = viewModel.uiState.name,
+                  type = viewModel.uiState.type,
+              )
 
-        viewModel.addMedication(newMed,context)
-      },
-      enabled = true,
+          viewModel.addMedication(newMed, context)
+        },
+        enabled = true,
     )
 
     HIGButton(
-      text = "Get Data",
-      onClick = {
+        text = "Get Data",
+        onClick = {
 
-        // val newMed = TEMP_Medication(uid = uid, name = medName.value, type = medType.value)
-        runBlocking {
-          launch {
-            viewModel.getMedsList()
-          }
-        }
-
-      },
-      enabled = true,
+          // val newMed = TEMP_Medication(uid = uid, name = medName.value, type = medType.value)
+          runBlocking { launch { viewModel.getMedsList() } }
+        },
+        enabled = true,
     )
 
     LazyColumn(
-      verticalArrangement = Arrangement.spacedBy(8.dp),
-      horizontalAlignment = Alignment.CenterHorizontally,
-      modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize(),
     ) {
 
       // TODO: get medications from firebase.
       items(meds) { medication ->
         CardComponent(
-          header = medication.type,
-          topEndText = "Edit",
-          content = medication.name,
-
-          onClick = {
-            // TODO: Реализовать открытие экрана с выбранным medication.
-          }
-        )
+            header = medication.type,
+            topEndText = "Edit",
+            content = medication.name,
+            onClick = {
+              // TODO: Реализовать открытие экрана с выбранным medication.
+            })
       }
     }
   }
-
-
 }
 
 @Composable
 fun CardComponent(
-  header: String,
-  topEndText: String?,
-  content: String,
-  onClick: () -> Unit,
-  modifier: Modifier = Modifier
+    header: String,
+    topEndText: String?,
+    content: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
   ElevatedCard(modifier = modifier) {
     Column(
-      modifier =
-      Modifier.padding(
-        horizontal = dimensionResource(R.dimen.card_padding_horizontal),
-        vertical = dimensionResource(R.dimen.card_padding_vertical)
-      ),
-      verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium))
-    ) {
-      Row(modifier = Modifier.fillMaxWidth()) {
-        Text(header)
-        Spacer(modifier.weight(1f))
-        NavigationRow({ onClick() }, topEndText ?: "")
-      }
-      Text(content, style = MaterialTheme.typography.headlineMedium)
-    }
+        modifier =
+            Modifier.padding(
+                horizontal = dimensionResource(R.dimen.card_padding_horizontal),
+                vertical = dimensionResource(R.dimen.card_padding_vertical)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium))) {
+          Row(modifier = Modifier.fillMaxWidth()) {
+            Text(header)
+            Spacer(modifier.weight(1f))
+            NavigationRow({ onClick() }, topEndText ?: "")
+          }
+          Text(content, style = MaterialTheme.typography.headlineMedium)
+        }
   }
 }
 
 @Composable
 fun NavigationRow(onClick: () -> Unit, label: String) {
   Row(
-    modifier = Modifier.clickable(onClick = onClick),
-    verticalAlignment = Alignment.CenterVertically,
-    horizontalArrangement = Arrangement.spacedBy(4.dp)
-  ) {
-    Text(label, style = MaterialTheme.typography.labelSmall)
-    Icon(
-      imageVector = Icons.AutoMirrored.Filled.ArrowForwardIos,
-      contentDescription = null,
-      tint = MaterialTheme.colorScheme.secondary,
-      modifier = Modifier.size(10.dp)
-    )
-  }
+      modifier = Modifier.clickable(onClick = onClick),
+      verticalAlignment = Alignment.CenterVertically,
+      horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+        Text(label, style = MaterialTheme.typography.labelSmall)
+        Icon(
+            imageVector = Icons.AutoMirrored.Filled.ArrowForwardIos,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.secondary,
+            modifier = Modifier.size(10.dp))
+      }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerModalInput(onDateSelected: (Long?) -> Unit, onDismiss: () -> Unit) {
+  val datePickerState = rememberDatePickerState(initialDisplayMode = DisplayMode.Input)
+
+  DatePickerDialog(
+      onDismissRequest = onDismiss,
+      confirmButton = {
+        TextButton(
+            onClick = {
+              onDateSelected(datePickerState.selectedDateMillis)
+              onDismiss()
+            }) {
+              Text("OK")
+            }
+      },
+      dismissButton = { TextButton(onClick = onDismiss) { Text("Cancel") } }) {
+        DatePicker(state = datePickerState)
+      }
+}
+
+@Preview
+@Composable
+fun CardComponentPreview() {
+  DatePickerModalInput({}, {})
 }
