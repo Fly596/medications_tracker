fb6a302c552011b22ba1822bd46c0c5698b8bd06
Fly596 <69361885+Fly596@users.noreply.github.com>
2024-12-07 03:24:44 +0300

    feat: Update profile screen
    This commit updates the profile screen to include profile image, name and email.
    The profile screen now shows a heading with user's name and email below the profile picture.
    It also updates profile option buttons and adds a logout button.

--- medicationstracker/src/main/java/com/galeria/medicationstracker/ui/screens/profile/ProfileScreen.kt
+++ medicationstracker/src/main/java/com/galeria/medicationstracker/ui/screens/profile/ProfileScreen.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,62 +22,108 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.galeria.medicationstracker.R
+import com.galeria.medicationstracker.ui.components.FlyTextButton
 import com.galeria.medicationstracker.ui.components.HIGButton
 import com.galeria.medicationstracker.ui.components.HIGButtonStyle
 import com.galeria.medicationstracker.ui.components.HIGListButton
+import com.galeria.medicationstracker.ui.screens.medications.update.UpdateMedVM
 import com.galeria.medicationstracker.ui.theme.MedTrackerTheme
 
 @Composable
-fun ProfileScreen(modifier: Modifier = Modifier) {
+fun ProfileScreen(
+  modifier: Modifier = Modifier,
+  viewModel: ProfileVM = viewModel(),
+) {
+  val state = viewModel.uiState
+
   Column(
     modifier = modifier.fillMaxSize(),
-    verticalArrangement = Arrangement.Top,
-    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.spacedBy(8.dp),
+    // horizontalAlignment = Alignment.CenterHorizontally,
   ) {
-    // Heading.
-    Text(text = "Profile", style = MedTrackerTheme.typography.title1)
 
-    Spacer(modifier = Modifier.height(24.dp))
 
-    PfpWithName(
-      painter = R.drawable.img_1543, // TODO: get and set using firebase.
-      userName = "User Name", // TODO: get from firebase.
+    // Screen header with title
+    Text(
+      "My Profile",
+      style = MedTrackerTheme.typography.largeTitleEmphasized,
     )
 
+    // Space between header and profile info.
+    Spacer(modifier = Modifier.padding(8.dp))
+
+    // User's profile picture and name.
+    PfpWithName(
+      // TODO: get from firebase.
+      painter = R.drawable.img_1543,
+      userName = "Adolf Hitler",
+      userEmail = "fly.yt.77@gmail.com"
+    )
+    // Space between profile info and options.
     Spacer(modifier = Modifier.height(24.dp))
 
-    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.Start) {
-      ProfileOptionItem("Details") {
-        // TODO: navigate to details screen.
-      }
+    // Profile options.
+    Column(
+      modifier = Modifier.fillMaxWidth(),
+      horizontalAlignment = Alignment.Start
+    ) {
+
+      // Notifications option.
       ProfileOptionItem("Notifications") {
         // TODO: navigate to notifications screen.
       }
+      // Details option.
+      ProfileOptionItem("Settings") {
+        // TODO: navigate to details screen.
+      }
+    }
 
-      HIGButton(onClick = {}, text = "Logout", style = HIGButtonStyle.Bezeled)
+    // Logout button.
+    FlyTextButton(onClick = {}) {
+      Text(text = "Logout")
     }
   }
 }
 
 @Composable
-fun PfpWithName(painter: Int, userName: String, modifier: Modifier = Modifier) {
-  Column(
-    horizontalAlignment = Alignment.CenterHorizontally,
-    verticalArrangement = Arrangement.Top,
+fun PfpWithName(
+  painter: Int,
+  userName: String,
+  userEmail: String,
+  modifier: Modifier = Modifier
+) {
+  Row(
+    verticalAlignment = Alignment.CenterVertically,
+    // horizontalAlignment = Alignment.CenterHorizontally,
+    // verticalArrangement = Arrangement.Top,
     modifier = modifier,
   ) {
     Image(
       painter = painterResource(painter),
       contentDescription = "pfp",
       contentScale = ContentScale.Crop,
-      modifier = modifier.clip(CircleShape).size(128.dp),
+      modifier = modifier
+        .clip(CircleShape)
+        .size(128.dp),
     )
 
-    Spacer(modifier = Modifier.height(12.dp))
+    Spacer(modifier = Modifier.width(16.dp)) // add default dp values.
 
-    Text(text = userName, style = MedTrackerTheme.typography.title2)
-    Text(text = userName, style = MedTrackerTheme.typography.title2)
+    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+      Text(
+        text = userName,
+        style = MedTrackerTheme.typography.title1,
+        color = MedTrackerTheme.colors.primaryLabel
+      )
+      Text(
+        text = userEmail,
+        style = MedTrackerTheme.typography.headline,
+        color = MedTrackerTheme.colors.secondaryLabel
+      )
+    }
+
   }
 }
 
@@ -83,7 +131,8 @@ fun PfpWithName(painter: Int, userName: String, modifier: Modifier = Modifier) {
 fun ProfileOptionItem(title: String, onClick: () -> Unit) {
   Row(
     modifier =
-      Modifier.fillMaxWidth()
+      Modifier
+        .fillMaxWidth()
         // .background(color =
         // MedicationsTrackerAppTheme.systemColors.backgroundLightSecondary)
         .clickable(onClick = onClick),
--- medicationstracker/src/main/java/com/galeria/medicationstracker/ui/screens/profile/ProfileVM.kt
+++ medicationstracker/src/main/java/com/galeria/medicationstracker/ui/screens/profile/ProfileVM.kt
@@ -1,11 +1,23 @@
 package com.galeria.medicationstracker.ui.screens.profile
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.galeria.medicationstracker.ui.screens.medications.update.UpdateMedUiState
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.firestore
 
-class ProfileViewModel : ViewModel() {
+data class ProfileScreenUiState(
+    val testData: String = ""
+)
+
+class ProfileVM : ViewModel() {
+
+    var uiState by mutableStateOf(ProfileScreenUiState())
+        private set
+
 
     private lateinit var auth: FirebaseAuth
 
