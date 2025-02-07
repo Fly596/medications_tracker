3aff1b53041cde64cccd6d14657363db485cc659
Fly596 <69361885+Fly596@users.noreply.github.com>
2025-02-07 22:32:47 +0300

    чуть новых цветов

--- medicationstracker/src/main/java/com/galeria/medicationstracker/ui/theme/AppColors.kt
+++ medicationstracker/src/main/java/com/galeria/medicationstracker/ui/theme/AppColors.kt
@@ -2,22 +2,29 @@ package com.galeria.medicationstracker.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val gprimaryDark = Color(0xFF121523)
+val gprimary = Color(0xFF30385c)
+val gprimaryLight = Color(0xFF4e5b96)
+
+val gbackground = Color(0xffffffff)
+val gbackgroundSecondary = Color(0xfffcfcff)
+val gbackgroundTertiary = Color(0xffe3e3e6)
+
+// todo: labels and syscolors
+
 val primary400 = Color(0xFF0A84FF)
 val primary500 = Color(0xFF0772de)
 val primary600 = Color(0xFF0560be)
 val primaryTinted = Color(0x260a84ff)
 val primaryLight = Color(0xFFEAF7FC)
-
 val secondary400 = Color(0xFF633957)
 val secondary500 = Color(0xFF55304b)
 val secondary600 = Color(0xFF47283e)
 val secondaryTinted = Color(0x26633957)
 val secondaryLight = Color(0xFF665360)
-
 val primaryFill = Color(0x33787880)
 val secondaryFill = Color(0x29787880)
 val tertiaryFill = Color(0x1f787880)
-
 val primaryBackground = Color(0xFFFFFFFF)
 val secondaryBackground = Color(0xFFF2F2F7)
 val tertiaryBackground = Color(0xFFFFFFFF)
@@ -26,7 +33,6 @@ val tertiaryBackground = Color(0xFFFFFFFF)
 val primaryBackgroundGrouped = Color(0xFFf2f2f7)
 val secondaryBackgroundGrouped = Color(0xFFffffff)
 val tertiaryBackgroundGrouped = Color(0xFFf2f2f7)
-
 val primaryBackgroundDark = Color(0xFF000000)
 val secondaryBackgroundDark = Color(0xFF1C1C1E)
 val tertiaryBackgroundDark = Color(0xFF000000)
@@ -37,22 +43,19 @@ val secondaryGroupedBackgroundDark = Color(0xFF000000)
 val tertiaryGroupedBackgroundDark = Color(0xFF1C1C1E)
 
 // Typography.
-val primaryLabel =
-  Color(
-    0xff000000
-  ) // for highlighting important text or content that needs to be displayed prominently to the
-
+// for highlighting important text or content that needs to be displayed prominently to the
 // user. For example, you can use primary color for titles and headlines, labels of the pages in
 // the navigation bars, field labels, or input control in forms.
-val secondaryLabel =
-  Color(0x99000000) // for displaying captions or supporting information. A secondary label can be used in forms and
+val primaryLabel = Color(0xff000000)
 
+// for displaying captions or supporting information. A secondary label can be used in forms and
 // input fields to provide additional context or instructions for fields or input controls, and
 // in lists or tables to display secondary information and details about a list or table items.
-val tertiaryLabel =
-  Color(0x4D000000) // for displaying even less important text or content, such as supplementary information or
-// details that are not essential to the user’s understanding of the app.
+val secondaryLabel = Color(0x99000000)
 
+// for displaying even less important text or content, such as supplementary information or
+// details that are not essential to the user’s understanding of the app.
+val tertiaryLabel = Color(0x4D000000)
 val primaryLabelDark = Color(0xffffffff)
 val secondaryLabelDark = Color(0x99ffffff)
 val tertiaryLabelDark = Color(0x4dffffff)
