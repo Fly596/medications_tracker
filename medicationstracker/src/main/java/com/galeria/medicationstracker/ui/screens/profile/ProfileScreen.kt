52d528f4ea2027c5b70fd60dce0d16dc4e638b0b
Fly596 <69361885+Fly596@users.noreply.github.com>
2025-02-17 19:37:49 +0300

    .

--- medicationstracker/src/main/java/com/galeria/medicationstracker/ui/screens/profile/ProfileScreen.kt
+++ medicationstracker/src/main/java/com/galeria/medicationstracker/ui/screens/profile/ProfileScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.galeria.medicationstracker.R
@@ -149,3 +150,32 @@ fun ProfileScreen(
     }
 }
 
+@Composable
+fun AccountScreen(
+    modifier: Modifier = Modifier,
+    ) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        AccountScreenHead()
+    }
+    
+}
+
+@Composable
+fun AccountScreenHead(
+    modifier: Modifier = Modifier,
+    
+) {
+    Row(modifier = modifier){
+        Text(
+            text = "My Account"
+        )
+    }
+}
+
+@Composable
+@Preview
+fun AccScreenPreview() {
+    MedTrackerTheme {
+        AccountScreen()
+    }
+}
