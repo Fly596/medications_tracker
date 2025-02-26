7829f021e033752594b05901e9e372d49bbf041e
Fly596 <69361885+Fly596@users.noreply.github.com>
2025-02-26 15:32:02 +0300

    nm

---
+++ medicationstracker/src/main/java/com/galeria/medicationstracker/ui/screens/profile/settings/profiledetails/ProfileDetailsScreen.kt
@@ -0,0 +1,9 @@
+package com.galeria.medicationstracker.ui.screens.profile.settings.profiledetails
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProfileDetailsScreen(modifier: Modifier = Modifier) {
+
+}
